f4fca85350b0243aad8fb08c4a8fc68673d3efd5
mahbubrmn99@gmail.com <[mahbubrmn99@gmail.com]>
2022-01-02 14:52:08 +0600

    first commit

---
+++ 2.cpp
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+    printf("Today is sunday.");
+    return 0;
+}
+
---
+++ 3.cpp
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+    printf("Today is Sunday.");
+    printf("loreme ipsume");
+    return 0;
+}
